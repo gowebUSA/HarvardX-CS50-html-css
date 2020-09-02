8b65e975472f267168849fe77cdc74f7d8f98fcb
gwebUSA <60155239+gowebUSA@users.noreply.github.com>
2020-09-02 16:40:24 -0700

    creates files up to greet.html

--- Lecture-4-Django/lecture3/hello/urls.py
+++ Lecture-4-Django/lecture3/hello/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path    #import is like Using Statement in C#.
 
 from . import views #dot (.) is from a current directory.
 
 urlpatterns = [
     path("", views.index, name="index"),    #if left blank on browser, it will use index bec path("",) is blank.
+    path("<str:name>", views.greet, name="greet"),
     path("richard", views.richard, name="richard"), #this will use /hello/richard
     path("ruby", views.ruby, name="ruby"),
 ]   
--- Lecture-4-Django/lecture3/hello/views.py
+++ Lecture-4-Django/lecture3/hello/views.py
@@ -1,12 +1,33 @@
-from django.http import HttpResponse
+from django.http import HttpResponse    #import the ability fro HttpResponse.This is like using Using Statement in C#
 from django.shortcuts import render
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse("Hello, world!")
+
+# def richard(request):
+#     return HttpResponse("Hello, Richard!")
+
+# def ruby(request):
+#     return HttpResponse("Hello, Ruby!")
+
+# def greet(request, name):
+#     return HttpResponse(f"Hello, {name.capitalize()}")
+
+# Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
 
 def richard(request):
     return HttpResponse("Hello, Richard!")
 
 def ruby(request):
     return HttpResponse("Hello, Ruby!")
+
+# def greet(request, name):
+#     return HttpResponse(f"Hello, {name.capitalize()}")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name":  name.capitalize()   #this is a python dictionary, key/value pair
+    })

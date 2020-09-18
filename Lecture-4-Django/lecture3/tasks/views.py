c8a47e7e9d87548e66dae1114e3a6676dd8a4e6c
Richard Go <60155239+gowebUSA@users.noreply.github.com>
2020-09-17 21:15:10 -0700

    Created tasks folder

---
+++ Lecture-4-Django/lecture3/tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+tasks = ["foo", "bar", "baz"]
+
+# Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })

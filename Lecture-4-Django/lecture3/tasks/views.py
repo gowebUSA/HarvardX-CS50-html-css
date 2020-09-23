ed0a1f9742223b516e054a74577116253d0aa591
Richard Go <60155239+gowebUSA@users.noreply.github.com>
2020-09-23 12:30:18 -0700

    Creates CSRF

--- Lecture-4-Django/lecture3/tasks/views.py
+++ Lecture-4-Django/lecture3/tasks/views.py
@@ -1,6 +1,11 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priotity = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -8,4 +13,6 @@ def index(request):
         "tasks": tasks
     })
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })

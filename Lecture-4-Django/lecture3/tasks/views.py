7c6a5f24030ea957f2afc6ccc069047add1b27bd
Richard Go <60155239+gowebUSA@users.noreply.github.com>
2020-09-23 18:21:04 -0700

    django.http import HttpResponseRedirect

--- Lecture-4-Django/lecture3/tasks/views.py
+++ Lecture-4-Django/lecture3/tasks/views.py
@@ -1,11 +1,13 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priotity = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    #priotity = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -13,6 +15,17 @@ def index(request):
         "tasks": tasks
     })
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("task:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
